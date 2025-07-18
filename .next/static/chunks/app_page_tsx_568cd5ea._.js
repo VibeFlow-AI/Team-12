2fe8888ec4e02e5026f85d3e61b77b482ef0208e
NayanthaNethsara <nayanthanethsara@gmail.com>
2025-07-18 19:47:07 +0530

    Stop tracking .next build folder

--- .next/static/chunks/app_page_tsx_568cd5ea._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_67bf7763._.js",
-  "static/chunks/_d1b1f0ed._.js"
-],
-    source: "dynamic"
-});
